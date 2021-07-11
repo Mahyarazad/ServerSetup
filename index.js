b689740431596a873cc8a1b9b565df88e6e2d07a
Maahyar Azad <39150387+Mahyarazad@users.noreply.github.com>
2021-07-11 17:41:34 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-const http = require('http');
+const https = require('https');
+const enforce = require('express-sslify');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
@@ -14,7 +15,7 @@ const appSecret = process.env.APP_SECRET;
 // mongoose.connect('mongodb://localhost/auth');
 
 //Dev Setup
-//
+
 mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@clusterzero.jn5oj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
  {useNewUrlParser: true},
  {useUnifiedTopology: true});
@@ -30,11 +31,16 @@ app.use(session({
 app.use(express.json())
 app.use(morgan('combined'));
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
-const server = http.createServer(app);
-server.listen(port);
-console.log('Server listening on:', port);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+  // const server = http.createServer(app);
+// server.listen(port);
+// console.log('Server listening on:', port);
