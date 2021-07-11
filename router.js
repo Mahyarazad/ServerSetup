33d533cf4119c143d5413712674a884616c70c85
Maahyar Azad <39150387+Mahyarazad@users.noreply.github.com>
2021-07-11 17:24:39 +0430

    Update router.js

--- router.js
+++ router.js
@@ -1,14 +1,12 @@
 const Authentication = require('./controllers/authentication');
+const TransactionProccess = require('./controllers/transactionProccess');
 const passportService = require('./services/passport');
 const passport = require('passport');
-
 const requireAuth = passport.authenticate('jwt', {session:false});
 const requireLogin = passport.authenticate('local', {session:false});
 
 module.exports = function(app){
     app.post('/signup', Authentication.signup)
-    app.post('/signin', requireLogin ,Authentication.signin)
-    app.get('/', requireAuth, function(req, res, next){
-        res.send({hi:'there'})
-    })
+    app.post('/signin', requireLogin , Authentication.signin)
+    app.post('/cart',TransactionProccess.purchase)
 };
