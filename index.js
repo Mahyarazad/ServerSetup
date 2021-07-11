a21fd27d46cf88cb9643a67c38d0dead01522cbc
Maahyar Azad <39150387+Mahyarazad@users.noreply.github.com>
2021-07-11 16:36:48 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -8,9 +9,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 // DB Setup
-mongoose.connect('mongodb://localhost/auth');
+// mongoose.connect('mongodb://localhost/auth');
+
+//Dev Setup
+
+mongoose.connect("mongodb+srv://MaahyarAzad:SagPedar%409508@clusterzero.jn5oj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+ {useNewUrlParser: true},
+ {useUnifiedTopology: true});
+
 
 // App Setup
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret:"HelloWorld"
+}));
+
+app.use(express.json())
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
